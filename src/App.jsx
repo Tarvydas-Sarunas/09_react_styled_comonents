534e2e4768f96220398cf944c62f13454780ea57
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-06 07:12:07 +0100

    global Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import './styles/App.css';
+import GlobalStil from './styles/GlogalStil';
 
 const MainTitle = styled.h1`
   text-transform: uppercase;
@@ -9,29 +10,39 @@ const MainTitle = styled.h1`
 
 const MyButton = styled.button`
 ${(props) => console.log('props ===', props)}
-background: black;
-color: white;
+background: ${(props) => props.$outline ? 'white' : 'black' };
+color: ${(props) => props.$outline ? 'black' : 'white' };
 border: 1px solid black;
 padding: 5px 10px;
 font-size: 17px;
 cursor: pointer;
 border-radius: 5px;
 &:hover {
-  background-color: #3c3a3a;
+  ${(props) => props.$outline ? '#ccc' : '#3c3a3a' }; ;
 }
 &:active {
   background-color: tomato;
 }
+`
+const RedBtn = styled(MyButton)`
+  background-color: tomato;
+  border-color: tomato;
+  &:hover {
+    background-color: #f7a395;
+  }
 `
 
 export default function App() {
   console.log('App susikure');
   return (
     <div className='App container'>
+      <GlobalStil />
       <MainTitle>Hello, world!</MainTitle>
       <hr />
       <MyButton>Click Me</MyButton>
       <MyButton $outline>Outline btn</MyButton>
+      <RedBtn $outline>Tomato btn</RedBtn>
+      <RedBtn as={'a'} href='#kazkur'>I am link btn</RedBtn>
     </div>
   );
 }
